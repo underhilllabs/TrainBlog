5ff9b28b2b644e26f5126c85045a6ae455bdc303
Bart Lantz <underhilllabs@gmail.com>
2019-12-05 06:51:30 -0700

    adds after create callback to create player profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  after_create :initialize_player_profile
   has_many :session_reports, class_name: 'SessionReport', foreign_key: :creator_id
-  has_one :player_profile
+  has_one :player_profile, dependent: :destroy
+
+  def to_s
+    "#{username}"
+  end
+  private
+  def initialize_player_profile
+    self.player_profile = PlayerProfile.create(user_id: self.id)
+  end
 end

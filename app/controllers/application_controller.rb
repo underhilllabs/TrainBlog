da2cfdf25060b2217fb6b3738bd2398c802f8a16
Bart Lantz <underhilllabs@gmail.com>
2019-12-12 07:25:42 -0700

    Adds pundit for authorizing access to resources and pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  include Pundit
+  protect_from_forgery
+
+  rescue_from Pundit::NotAuthorizedError do |exception|
+    policy_name = exception.policy.class.to_s.underscore
+    redirect_to new_user_session_url, alert: 'You must create an account to do this.'
+  end
 end

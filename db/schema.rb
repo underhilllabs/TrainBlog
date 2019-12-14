90ab2d9b36c804d4996042f24a0d5d6a70ec324f
Bart Lantz <underhilllabs@gmail.com>
2019-12-14 08:32:31 -0700

    adds redcarpet for markdown support
    I decided to add an optional markdown section to posts and session reports as ActionText is too limiting

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_30_185922) do
+ActiveRecord::Schema.define(version: 2019_12_13_233038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 2019_11_30_185922) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "category"
+    t.text "body"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -96,6 +97,7 @@ ActiveRecord::Schema.define(version: 2019_11_30_185922) do
     t.integer "creator_id"
     t.text "final_stat"
     t.text "player_manifest"
+    t.text "body"
   end
 
   create_table "users", force: :cascade do |t|

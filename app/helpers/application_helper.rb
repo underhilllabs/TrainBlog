0ef58ed601a17bdb53cdcbaf77b5b1ef967db323
Bart Lantz <underhilllabs@gmail.com>
2019-12-18 09:30:00 -0700

    Adds validation to Session Reports
    and also checks date is present in fancy_date helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper\
   def nice_date_form(the_date)
-    return the_date.strftime('%b %e, %Y')
+    if the_date.present?
+      the_date.strftime('%b %e, %Y')
+    else
+      "No date"
+    end
   end
 end
--- app/models/session_report.rb
+++ app/models/session_report.rb
@@ -1,5 +1,7 @@
 class SessionReport < ApplicationRecord
   has_rich_text :content
+  validates :title, presence: true
+  validates :game_date, presence: true
   belongs_to :creator, class_name: 'User'
   has_many :comments, as: :commentable
   #belongs_to :game, optional: true

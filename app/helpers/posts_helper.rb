33338086eb0bbd7cee75c4fa9863410479fecb9c
Bart Lantz <underhilllabs@gmail.com>
2019-12-18 09:06:42 -0700

    Adds new markdown section
    instead of only allowing use of ActionText which is quite limited:
    no tables, no different headers

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,9 @@
 module PostsHelper
+  def markdown
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+      no_intra_emphasis: true, 
+      fenced_code_blocks: true,   
+      disable_indented_code_blocks: true)
+    return markdown.render(text).html_safe
+  end
 end

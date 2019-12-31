ee49343be71a6023f100925c97afe93604c17f57
Bart Lantz <underhilllabs@gmail.com>
2019-12-31 15:44:54 -0700

    adds more fields to game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,9 @@
 class Game < ApplicationRecord
+  has_rich_text :content
+  validates :title, presence: true
   has_many :session_reports
   has_one_attached :banner
+  def to_s
+    "#{title}"
+  end
 end

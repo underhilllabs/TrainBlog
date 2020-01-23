696b8957fdfa704025853e68a659fe85198dae94
Bart Lantz <underhilllabs@gmail.com>
2020-01-23 09:04:27 -0700

    Updates session reports on home page to latest 10

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @games = Game.order(:title)
-    @session_reports = SessionReport.order(game_date: :DESC).all
+    @session_reports = SessionReport.published.order(game_date: :ASC).last(10).reverse
     # show sidebars
     render layout: 'main_with_sidebar'
   end

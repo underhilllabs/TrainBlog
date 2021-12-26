f352e5ba905faac7039d4435e3ef499402d6666f
Bart Lantz <underhilllabs@gmail.com>
2021-12-26 10:28:01 -0700

    deletes unused parts of pundit unauthorized error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,7 @@ class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery
 
-  rescue_from Pundit::NotAuthorizedError do |exception|
-    policy_name = exception.policy.class.to_s.underscore
+  rescue_from Pundit::NotAuthorizedError
     redirect_to new_user_session_url, alert: 'You must create an account to do this.'
   end
 end

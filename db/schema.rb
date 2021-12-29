abaadc42321a9b3c67cb9b524d5e40c9f728bff0
Bart Lantz <underhilllabs@gmail.com>
2021-12-29 08:13:16 -0700

    adds rubocop configuration

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_26_151440) do
-
+ActiveRecord::Schema.define(version: 20_211_226_151_440) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
